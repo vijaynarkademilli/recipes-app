7dcd268fcf02356cb91ce2574009903777a231bf
vijay narkademilli <vijaykumar.narkademilli@gmail.com>
2019-11-10 16:00:22 +0530

    Initial WebService commit

---
+++ src/test/java/com/recipes/app/recipesapp/RecipesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipes.app.recipesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
