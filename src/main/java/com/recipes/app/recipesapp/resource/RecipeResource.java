9e22ae824ef4d1f2bbccb461decd188180e4dc01
vijay narkademilli <vijaykumar.narkademilli@gmail.com>
2019-11-11 21:14:13 +0530

    Adding cross origin to allow request from angular

--- src/main/java/com/recipes/app/recipesapp/resource/RecipeResource.java
+++ src/main/java/com/recipes/app/recipesapp/resource/RecipeResource.java
@@ -3,6 +3,7 @@ package com.recipes.app.recipesapp.resource;
 import com.recipes.app.recipesapp.model.Recipe;
 import com.recipes.app.recipesapp.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import java.util.List;
  */
 
 @RestController
+@CrossOrigin("*")
 public class RecipeResource {
 
     @Autowired
