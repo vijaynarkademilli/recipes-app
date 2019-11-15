77f9283c5d74241d37d5ea2a75e226ebe2b4e3cd
vijay narkademilli <vijaykumar.narkademilli@gmail.com>
2019-11-15 06:27:38 +0530

    Http Basic Authentication changes

---
+++ src/main/java/com/recipes/app/recipesapp/security/SpringSecurityConfigurationBasicAuth.java
@@ -0,0 +1,27 @@
+package com.recipes.app.recipesapp.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by madhuri on 15-11-2019.
+ */
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .csrf().disable()
+        .authorizeRequests()
+        .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .httpBasic();
+    }
+
+}
